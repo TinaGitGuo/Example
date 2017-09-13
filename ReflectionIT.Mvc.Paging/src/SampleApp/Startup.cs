aee55e4d8cbbd13a135362d3043845995b1fd7de
TinaGitGuo <840322864@qq.com>
2017-09-13 14:50:11 +0800

    Adding RReflectionIT.Mvc.Paging
    Adding RReflectionIT.Mvc.Paging

---
+++ ReflectionIT.Mvc.Paging/src/SampleApp/Startup.cs
@@ -0,0 +1,59 @@
+﻿using ReflectionIT.Mvc.Paging;
+using SampleApp.Models.Database;
+using SampleApp.Services;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleApp {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<NorthwindContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Northwind")));
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            // Register ViewComponent using an EmbeddedFileProvider & setting some options
+            services.AddPaging(options => {
+                options.ViewName = "Bootstrap4";
+                options.HtmlIndicatorDown = " <span>&darr;</span>";
+                options.HtmlIndicatorUp = " <span>&uarr;</span>";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            } else {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ ReflectionIT.Mvc.Paging/src/SampleApp/ViewComponents/TestThingViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApp.ViewComponents
+{
+    public class TestThingViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(string textData)
+        {
+            await Task.Delay(1);
+
+            return View(model: textData);
+        }
+
+    }
+}
